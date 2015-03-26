b9ff6681070be5118119fdaedcf846710dbb4c3b
BeaulieuLoic <beaulielloic@gmail.com>
2015-03-26 10:38:40 +0100

    add commande line for appli

--- src/WebManager.py
+++ src/WebManager.py
@@ -1,10 +1,25 @@
 import cherrypy
 import sqlite3
 from dataBase import DataBase
+import argparse
+import os
 
 pathDataBase = "dataBase.db"
 
 
+def set_path_data_base(path):
+    path_data_base = path
+    if not os.path.isfile(path):
+        print("Error: file don't exists !")
+        sys.exit()
+
+
+
+parser = argparse.ArgumentParser(description='web service.')
+parser.add_argument('path_data_base', metavar='path_data_base', type=str, help='set the data base path')
+
+args = parser.parse_args()
+set_path_data_base(args.path_data_base)
 
 class WebManager(object):
 
@@ -47,10 +62,8 @@ class WebManager(object):
 
     def get_name_of_column(self, table):
         """
-        return the name of all column of a table
+        return the name of all column in a table html
         """
-
-
         conn = sqlite3.connect(pathDataBase)
         c = conn.cursor()
         toReturn = "<tr>"
@@ -64,12 +77,27 @@ class WebManager(object):
         conn.close()
         return toReturn
 
+    def get_name_of_column_in_array(self, table):
+        """
+        return the name of all column in a array
+        """
+        conn = sqlite3.connect(pathDataBase)
+        c = conn.cursor()
+        toReturn = []
+        for row in c.execute("PRAGMA table_info('"+table+"')").fetchall():
+            tmp = 0
+            for x in row:
+                if (x.__class__.__name__ == "str") and (tmp%2 == 1):
+                    toReturn.append(x)
+                tmp  = tmp + 1
+        conn.close()
+        return toReturn
+
+
     def get_all_element_of_table(self, table):
         """
         return all element of table
         """
-
-
         conn = sqlite3.connect(pathDataBase)
         c = conn.cursor()
         toReturn = "<tr>"
@@ -148,14 +176,14 @@ class WebManager(object):
         """
         display form for search elements in data base
         """
+        toReturn = ""
 
-
-        toReturn = """ 
+        toReturn = toReturn +""" 
         <h1>Search something in table</h1>
             <h3>search by INSEE number</h3>
 
             <table border=\"1\">
-                <form method="get" action="search_equipment_INSEE">
+                <form method="get" action="search_equipment">
                     <tr>
                         <td>
                             Equipment
@@ -164,11 +192,21 @@ class WebManager(object):
                             <input type="text" name="searchEquipment">
                         </td>
                         <td>
+                            <select name="field">
+        """
+
+        for field in self.get_name_of_column_in_array("Equipment"):
+            toReturn = toReturn + "<option value="+str(field)+">"+str(field)+"</option>"
+        toReturn = toReturn +"""
+
+                            </select>
+                        </td>
+                        <td>
                             <input type="submit">
                         </td>
                     </tr>
                 </form>
-                <form method="get" action="search_activity_INSEE">
+                <form method="get" action="search_activity">
                     <tr>
                         <td>
                             Activity
@@ -177,17 +215,37 @@ class WebManager(object):
                             <input type="text" name="searchActivity">
                         </td>
                         <td>
+                            <select name="field">
+        """
+
+        for field in self.get_name_of_column_in_array("Activity"):
+            toReturn = toReturn + "<option value="+str(field)+">"+str(field)+"</option>"
+
+        toReturn = toReturn +"""
+                            </select>
+                        </td>
+                        <td>
                             <input type="submit">
                         </td>
                     </tr>
                 </form>
-                <form method="get" action="search_installation_INSEE">
+                <form method="get" action="search_installation">
                     <tr>
                         <td>
                             Installations
                         </td>
                         <td>
                             <input type="text" name="searchInstallation">
+                        </td>
+                        <td>
+                            <select name="field">
+        """
+
+        for field in self.get_name_of_column_in_array("Installations"):
+            toReturn = toReturn + "<option value="+str(field)+">"+str(field)+"</option>"
+        
+        toReturn = toReturn +"""
+                            </select>
                         </td>
                         <td>
                             <input type="submit">
@@ -200,38 +258,38 @@ class WebManager(object):
 
 
     @cherrypy.expose
-    def search_equipment_INSEE(self, searchEquipment):
+    def search_equipment(self, searchEquipment, field):
         """
         display elements in Equipment where insee contain searchEquipment
         """
         toReturn = ""
         toReturn = toReturn + "<table border=\"1\"> "
         toReturn = toReturn + self.get_name_of_column("Equipment")
-        toReturn = toReturn + self.search_element_of_table("Equipment", "comInsee", str(searchEquipment))
+        toReturn = toReturn + self.search_element_of_table("Equipment", field, str(searchEquipment))
         toReturn = toReturn + "</table>"
         return toReturn
 
     @cherrypy.expose
-    def search_activity_INSEE(self, searchActivity):
+    def search_activity(self, searchActivity, field):
         """
         display elements in Equipment where insee contain searchActivity
         """
         toReturn = ""
         toReturn = toReturn + "<table border=\"1\"> "
         toReturn = toReturn + self.get_name_of_column("Activity")
-        toReturn = toReturn + self.search_element_of_table("Activity", "inseeNb", str(searchActivity))
+        toReturn = toReturn + self.search_element_of_table("Activity", field, str(searchActivity))
         toReturn = toReturn + "</table>"
         return toReturn
 
     @cherrypy.expose
-    def search_installation_INSEE(self, searchInstallation):
+    def search_installation(self, searchInstallation, field):
         """
         display elements in Installations where insee contain searchInstallation
         """
         toReturn = ""
         toReturn = toReturn + "<table border=\"1\"> "
         toReturn = toReturn + self.get_name_of_column("Installations")
-        toReturn = toReturn + self.search_element_of_table("Installations", "INSEE", str(searchInstallation))
+        toReturn = toReturn + self.search_element_of_table("Installations", field, str(searchInstallation))
         toReturn = toReturn + "</table>"
         return toReturn
 
--- src/appli.py
+++ src/appli.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-
 from dataBase import DataBase
 from serialiser import Serializer
-
-
+import argparse
+import os
+import sys
+'''
+set default value for test appli
+'''
 path_installations_json = "../data/installations/installations.json"
 path_activity_json = "../data/activités/activités.json"
 path_equipment_json = "../data/equipements/equipements.json"
@@ -15,27 +18,92 @@ installation_Array = []
 activity_Array = []
 equipmentArray = []
 
+def set_path_data_base(path):
+	path_data_base = path
+	if args.init:
+		print("creation of data base and tables ...")
+		DataBase.init_data_base_installation(path_data_base)
+		DataBase.init_data_base_activity(path_data_base)
+		DataBase.init_data_base_equipment(path_data_base)
+	else:
+		if not os.path.isfile(path):
+			print("file don't exists, use --init for create data base")
+			sys.exit()
 
-'''
-creation of dataBase and table
-'''
-print("creation of data base and tables ...")
-DataBase.init_data_base_installation(path_data_base)
-DataBase.init_data_base_activity(path_data_base)
-DataBase.init_data_base_equipment(path_data_base)
+def set_path_json_installation():
+	path_installations_json=input('Enter the path of json file installations:')
 
-'''
-unserialise json file
-'''
-print("unserialise json files ...")
-installationArray = Serializer.unserialise_installations_json(path_installations_json)
-activityArray = Serializer.unserialise_activity_json(path_activity_json)
-equipmentArray = Serializer.unserialise_equipment_json(path_equipment_json)
+def set_path_json_activity():
+	path_activity_json=input('Enter the path of json file activity:')
 
-'''
-insert Array into dataBase
-'''
-print("insert array objects into data base ...")
-Serializer.object_export_in_dataBase(installationArray,path_data_base)
-Serializer.object_export_in_dataBase(activityArray,path_data_base)
-Serializer.object_export_in_dataBase(equipmentArray,path_data_base)
+def set_path_json_equipment():
+	path_equipment_json=input('Enter the path of json file equipment:')
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='insert object from json a files in data base.')
+
+	'''
+	args for set the path of data base 
+	'''
+	parser.add_argument('-i','--init', action='store_true', help='initialisation of data base')
+	parser.add_argument('path_data_base', metavar='path_data_base', type=str, help='set the data base path (if it isn\'t exists use --init )')
+
+	'''
+	args for set the path of json file for installations, activity, equipments
+	'''
+	parser.add_argument('-insJson','--installationsJson', action='store_true', help='set optional json file ')
+	parser.add_argument('-actJson','--activityJson', action='store_true', help='set optional json file ')
+	parser.add_argument('-equJson','--equipmentsJson', action='store_true', help='set optional json file ')
+
+
+	args = parser.parse_args()
+
+
+	set_path_data_base(args.path_data_base)
+
+
+	if args.installationsJson:
+		set_path_json_installation()
+		print("unserialise json installations and import to data base ...")
+		installationArray = Serializer.unserialise_installations_json(path_installations_json)
+		Serializer.object_export_in_dataBase(installationArray,path_data_base)
+
+	if args.activityJson:
+		set_path_json_activity()
+		print("unserialise json activity and import to data base ...")
+		activityArray = Serializer.unserialise_activity_json(path_activity_json)
+		Serializer.object_export_in_dataBase(activityArray,path_data_base)
+
+	if args.equipmentsJson:
+		set_path_json_equipment()
+		print("unserialise json equipments and import to data base ...")
+		equipmentArray = Serializer.unserialise_equipment_json(path_equipment_json)
+		Serializer.object_export_in_dataBase(equipmentArray,path_data_base)
+
+	"""
+	'''
+	creation of dataBase and table
+	'''
+	print("creation of data base and tables ...")
+	DataBase.init_data_base_installation(path_data_base)
+	DataBase.init_data_base_activity(path_data_base)
+	DataBase.init_data_base_equipment(path_data_base)
+
+	'''
+	unserialise json file
+	'''
+	print("unserialise json files ...")
+	installationArray = Serializer.unserialise_installations_json(path_installations_json)
+	activityArray = Serializer.unserialise_activity_json(path_activity_json)
+	equipmentArray = Serializer.unserialise_equipment_json(path_equipment_json)
+
+	'''
+	insert Array into dataBase
+	'''
+	print("insert array objects into data base ...")
+	Serializer.object_export_in_dataBase(installationArray,path_data_base)
+	Serializer.object_export_in_dataBase(activityArray,path_data_base)
+	Serializer.object_export_in_dataBase(equipmentArray,path_data_base)
+	"""
